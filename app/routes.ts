266d432f643a6bf00b277de2b17dddfce150f17c
wineAk <akasaka@interpark.co.jp>
2025-08-25 13:46:46 +0900

    [fix]

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  route("/test_wikipedia", "routes/layout.tsx", [
+  route("/test_wikipedia_v1", "routes/layout.tsx", [
     index("routes/home.tsx"),
     route(":name", "routes/wikipedia.tsx"),
   ]),
